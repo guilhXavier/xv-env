96a810ad4c4595c9fc3df46fc836be38733c8e39
guilherme.xavier <guilherme.xavier@sap.com>
2024-01-15 16:55:52 -0300

    refactor: removed functions

--- src/main.lisp
+++ src/main.lisp
@@ -2,15 +2,6 @@
   (:use :cl))
 (in-package :xv-env)
 
-(defparameter *environment-variables* '())
-
-(defun xv-env/read-lines (&key (path-to-file "./.env"))
-  "This function reads the lines of a enviroment variable file"
-  (let ((true-name (probe-file path-to-file)))
-    (if true-name
-        (uiop:read-file-lines true-name)
-        "File not found.")))
-
 (defun xv-env/split-lines (lines)
   "This function turns a list of lines into a list of key-value pairs"
   (mapcar (lambda (line)
@@ -21,15 +12,20 @@
   (mapcar (lambda (sublist)
             (cons (intern (car sublist) "KEYWORD") (cdr sublist))) list-of-lists))
 
-(defun xv-env/get-keywords (list-of-lists)
-  "This function returns a list of keywords"
-  (mapcar 'car list-of-lists))
+(defun xv-env/read-lines (&key (path-to-file "./.env"))
+  "This function reads the lines of a enviroment variable file"
+  (let ((true-name (probe-file path-to-file)))
+    (if true-name
+        (xv-env/build-dict (xv-env/split-lines (uiop:read-file-lines true-name)))
+        "File not found.")))
 
-(defun xv-env/get-values (list-of-lists)
-  "This function returns a list of values"
-  (mapcar 'cdr list-of-lists))
+(defun xv-env/build-lists (list-of-lists)
+  "This function builds a list containing a list of keys and a list of associated values"
+  (cons (mapcar 'car list-of-lists) (mapcar 'cdr list-of-lists)))
 
-(defun xv-env/test ()
-  (let ((lines (xv-env/read-lines :path-to-file "./.env")))
-    (let ((dict (xv-env/build-dict (xv-env/split-lines lines))))
-      (pairlis (xv-env/get-keywords dict) (xv-env/get-values dict)))))
+(defun get-variables (&optional (path))
+  (let ((lines (xv-env/read-lines :path-to-file path)))
+    (let ((keys (mapcar 'car lines))
+          (values (mapcar 'cdr lines)))
+      (pairlis keys values))))
+
